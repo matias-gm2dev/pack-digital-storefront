0eb132bc9843757ba994a794e8f77e5406e79100
M <matias.diaz@gm2dev.com>
2023-11-03 09:42:40 -0300

    initial commit dev

---
+++ app/sections/MyTestSection.tsx
@@ -0,0 +1,25 @@
+export const MyTestSection = ({cms}: any) => {
+  return (
+    <div>
+      <p>{cms.howdy}</p>
+    </div>
+  );
+};
+
+MyTestSection.Schema = () => {
+  return {
+    category: 'Heros',
+    label: 'MyTestSection',
+    key: 'my-test-section',
+    previewSrc:
+      'https://cdn.shopify.com/s/files/1/0671/5074/1778/files/text-block-preview.jpg?v=1675730349',
+    fields: [
+      {
+        label: 'Howdy',
+        name: 'howdy',
+        component: 'text',
+        defaultValue: '...',
+      },
+    ],
+  };
+};
